fd89dd050a73a0a609b261084df4486c0ac082c1
MrWooJ <hurricanc@gmail.com>
2016-11-14 12:54:25 +0330

    Add Diversity Check Function to Fitness Module

--- Workspace/Project/FitnessModule.js
+++ Workspace/Project/FitnessModule.js
@@ -5,5 +5,9 @@ module.exports =
     var score = 0
     // use your phenotype data to figure out a fitness score
     return score
-  }
+  },
+  diversityFunction: function(phenoTypeA, phenoTypeB)
+  {
+    // diversity check
+  },
 };
