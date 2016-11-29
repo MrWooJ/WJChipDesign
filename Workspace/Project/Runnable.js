1fff8db8b6b0c2035cd55de9a9b7113ac5d3c761
MrWooJ <hurricanc@gmail.com>
2016-11-29 09:53:50 +0330

    Enhance Functionality of Runnable Script
    * Handle Input Arguments

--- Workspace/Project/Runnable.js
+++ Workspace/Project/Runnable.js
@@ -1,6 +1,10 @@
 var prettyjson    = require('prettyjson')
+var serverModule  = require('./VisualOutput/ServerModule')
+var conf          = require('./VisualOutput/gf.json')
 var fileIOModule  = require('./FileIOModule')
 var IOFormatter   = require('./IOFormatter')
+var population    = require('./populationModule')
+var mainEngine    = require('./MainEngine')
 
 const commandLineArgs = require('command-line-args')
 
@@ -15,7 +19,7 @@ const optionDefinitions =
 
 const options = commandLineArgs(optionDefinitions)
 
-if (!options.input || !options.output || !options.number)
+if (!options.input || !options.output)
   throw new Error('Usage: node Runnable [--visual | -v] [--debug | -d] [--input | -v input.txt] [--output | -o output.txt] [--number | n #i]')
 
 var prettyjsonOptions = {
@@ -28,9 +32,41 @@ var prettyjsonOptions = {
 
 fileIOModule.readFromFile(options.input, function(input){
   IOFormatter.prepareInput(input, function(data){
-    // Pass to Main Engine
 
-    if (options.debug)
-      console.log(prettyjson.render(data[options.number], prettyjsonOptions))
+    if (options.number)
+    {
+      var population = population.populationFunction(data[options.number])
+      mainEngine.startEngine(population, function(result){
+
+        IOFormatter.prepareOutputText(result, function(result){
+          fileIOModule.writeInFile(options.output, result, function(error){})
+        })
+
+        if (options.visual)
+          serverModule.runServer(JSON.stringify(result))
+
+        if (options.debug)
+          console.log(prettyjson.render(result, prettyjsonOptions))
+      })
+    }
+    else
+    {
+      for (var i = 0; i < data.length; i++)
+      {
+        var population = population.populationFunction(data[i])
+        mainEngine.startEngine(population, function(result){
+
+          IOFormatter.prepareOutputText(result, function(result){
+            fileIOModule.writeInFile(options.output, result, function(error){})
+          })
+
+          if (options.visual)
+            serverModule.runServer(JSON.stringify(result))
+
+          if (options.debug)
+            console.log(prettyjson.render(result, prettyjsonOptions))
+        })
+      }
+    }
   })
 })
