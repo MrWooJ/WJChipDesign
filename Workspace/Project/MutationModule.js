4a70efea6ddb169e73057d0825a502a3087565b7
MrWooJ <hurricanc@gmail.com>
2016-11-29 09:51:28 +0330

    Implement Mutation Function to Toggle Direction of Transistor

--- Workspace/Project/MutationModule.js
+++ Workspace/Project/MutationModule.js
@@ -3,6 +3,14 @@ module.exports =
   mutationFunction: function(phenotype)
   {
     // make a random change to phenotype
+    var count = phenotype.nmos + phenotype.pmos
+    var randNumb = Math.floor(Math.random() * (count + 1))
+
+    if (randNumb < Math.floor(count/2))
+      phenotype.nmosTransistors[randNumb].srcLeft = !phenotype.nmosTransistors[randNumb].srcLeft
+    else
+      phenotype.pmosTransistors[count-randNumb].srcLeft = !phenotype.pmosTransistors[count-randNumb].srcLeft
+
     return phenotype
   }
 }
