3304ac39989bc7de6777f21ca24abe5852f80d3c
MrWooJ <hurricanc@gmail.com>
2016-12-02 18:01:45 +0330

    Enhance Functionality of Mutation Function

--- Workspace/Project/MutationModule.js
+++ Workspace/Project/MutationModule.js
@@ -3,13 +3,13 @@ module.exports =
   mutationFunction: function(phenotype)
   {
     // make a random change to phenotype
-    var count = phenotype.nmos + phenotype.pmos
-    var randNumb = Math.floor(Math.random() * (count + 1))
+    var count = Math.floor((phenotype.nmos + phenotype.pmos)/2)
+    var randNumb = Math.floor(Math.random() * count * 2)
 
-    if (randNumb < Math.floor(count/2))
+    if (randNumb < count)
       phenotype.nmosTransistors[randNumb].srcLeft = !phenotype.nmosTransistors[randNumb].srcLeft
     else
-      phenotype.pmosTransistors[count-randNumb].srcLeft = !phenotype.pmosTransistors[count-randNumb].srcLeft
+      phenotype.pmosTransistors[randNumb-count].srcLeft = !phenotype.pmosTransistors[randNumb-count].srcLeft
 
     return phenotype
   }
