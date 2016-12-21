6cc39dec1ccbe5b5b634a9f29a8c589741ccc692
MrWooJ <hurricanc@gmail.com>
2016-12-21 10:44:11 +0330

    Change Direction of Chip While Equality of Rand Numbers Otherwise Changing Position

--- Workspace/Project/MutationModule.js
+++ Workspace/Project/MutationModule.js
@@ -1,16 +1,28 @@
+var utility = require('./utility')
+
 module.exports =
 {
   mutationFunction: function(phenotype)
   {
     // make a random change to phenotype
-    var count = Math.floor((phenotype.nmos + phenotype.pmos)/2)
-    var randNumb = Math.floor(Math.random() * count * 2)
+    var clonePheno = JSON.parse(JSON.stringify(phenotype))
+    var choose = Math.floor((Math.random() * 10) + 1)
+    var randNumber1 = Math.floor((Math.random() * (phenotype.nmos)))
+    var randNumber2 = Math.floor((Math.random() * (phenotype.nmos)))
 
-    if (randNumb < count)
-      phenotype.nmosTransistors[randNumb].srcLeft = !phenotype.nmosTransistors[randNumb].srcLeft
+    if (randNumber1 == randNumber2)
+    {
+      if (choose <= 5)
+        clonePheno.nmosTransistors[randNumber1].srcLeft = clonePheno.nmosTransistors[randNumber1].srcLeft ? false : true
+      else
+        clonePheno.pmosTransistors[randNumber1].srcLeft = phenotype.pmosTransistors[randNumber1].srcLeft ? false : true
+    }
     else
-      phenotype.pmosTransistors[randNumb-count].srcLeft = !phenotype.pmosTransistors[randNumb-count].srcLeft
+    {
+      if (choose <= 5)
+        utility.move(clonePheno.nmosTransistors, randNumber1, randNumber2)
+    }
 
-    return phenotype
+    return clonePheno
   }
 }
